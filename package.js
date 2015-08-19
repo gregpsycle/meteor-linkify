0846a9f4f69a77971867b46361f82a9260fb0645
Lingxiao Sun <sunny9732@gmail.com>
2015-08-19 13:04:16 -0400

    updated package dependency

--- package.js
+++ package.js
@@ -1,22 +1,17 @@
 Package.describe({
-  name: 'meteor-linkify',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'lingxiaosun:meteor-linkify',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: 'JavaScript plugin for finding links in plain-text and converting them to HTML <a> tags.',
+	// URL to the Git repository containing the source code for this package.
+	git: 'https://github.com/sunny0825/meteor-linkify.git',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.addFiles('meteor-linkify.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('meteor-linkify');
-  api.addFiles('meteor-linkify-tests.js');
+	api.versionsFrom('1.1.0.3');
+	api.addFiles('lib/linkify/dist/jquery.linkify.js');
+	api.addFiles('lib/linkify/dist/jquery.linkify.min.js');
 });
